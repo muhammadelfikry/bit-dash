b1075330f67b6f1f76a0b5bc13eaa1b65029f558
Muhammad Elfikry <melfikry@gmail.com>
2025-06-15 04:30:41 +0700

    fix: fixes the output from prediction to label only

--- inference/sentiment_predictor_embedding.py
+++ inference/sentiment_predictor_embedding.py
@@ -12,20 +12,23 @@ tokenizer_path = "./models/embedding/tokenizer_embedding.pickle"
 with open(tokenizer_path, "rb") as handle:
     tokenizer = pickle.load(handle)
 
-label_map = {0: "netral", 1: "positif", 2: "negatif"}
+label_map = {0: "Neutral", 1: "Positive", 2: "Negative"}
 
 def predict(text):
+    if isinstance(text, str):
+        text = [text]
+
+    text = [t for t in text if t is not None]
+
+    if len(text) == 0:
+        return []
+
     sequences = tokenizer.texts_to_sequences(text)
     padded = pad_sequences(sequences, maxlen=100, padding="post")
     
     prediction = model.predict(padded)
-    predicted_class = np.argmax(prediction, axis=1)[0]  # Ambil index kelas dengan probabilitas tertinggi
-    confidence = np.max(prediction)
+    predicted_classes = np.argmax(prediction, axis=1)
 
-    result = {
-        "text": text,
-        "predicted_class": predicted_class,
-        "label": label_map[predicted_class],
-        "confidence": float(confidence)
-    }
-    return result
+    labels = [label_map[i] for i in predicted_classes]
+    
+    return labels[0] if len(labels) == 1 else labels
